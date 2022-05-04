bf524e264fff65f1f39a8b2e05f2c9814845ee81
sambetant <104916257+sambetant@users.noreply.github.com>
2022-05-04 17:10:33 +0200

    pxt.json, main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,3 +1,22 @@
+let strip: neopixel.Strip = null
+let kleur = 0
 basic.forever(function () {
-	
+    strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
+    strip.showColor(kleur)
+    kleur = kleur + 1
+    basic.pause(50)
+})
+basic.forever(function () {
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 100)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, -100)
+        basic.pause(100)
+    } else {
+        if (maqueen.Ultrasonic(PingUnit.Centimeters) < 10) {
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 100)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, -100)
+        } else {
+            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 100)
+        }
+    }
 })
